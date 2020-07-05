7affee62f86459dbad7de076e6051e6828a217dc
tyage <namatyage@gmail.com>
2020-07-05 16:16:55 +0900

    add base url

--- src/app/admin/flag/flag.service.ts
+++ src/app/admin/flag/flag.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import {PlatformLocation} from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlagService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private platformLocation: PlatformLocation) { }
 
   getFlag() {
-    return this.http.get('/admin-api/flag')
+    // XXX: prefix is not required if this option is added https://github.com/angular/angular/pull/37539/files
+    const {href, protocol, hostname, port} = this.platformLocation
+    const urlPrefix = `${protocol}//${hostname}:${port}`
+    const url = new URL('/admin-api/flag', urlPrefix)
+    return this.http.get(url.toString())
   }
 }
