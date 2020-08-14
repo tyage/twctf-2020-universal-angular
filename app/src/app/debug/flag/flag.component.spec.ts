2458f136d4ba1b922eccac0fe120ec3ff8bdeef2
tyage <namatyage@gmail.com>
2020-08-14 18:42:11 +0900

    change test to flag

--- app/src/app/debug/flag/flag.component.spec.ts
+++ app/src/app/debug/flag/flag.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FlagComponent } from './test.component';
+import { FlagComponent } from './flag.component';
 
 describe('FlagComponent', () => {
   let component: FlagComponent;
